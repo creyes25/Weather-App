362caf71dc169115ac54d8ba2deab30d2dc10fdc
creyes25 <carolina.reyes2022@gmail.com>
2024-04-22 03:23:39 -0500

    add components to be able to display data

---
+++ src/components/CityDetails/CityDetails.jsx
@@ -0,0 +1,40 @@
+import { Header } from "../Header/Header"
+
+import { WeatherCard } from "../WeatherCard/WeatherCard"
+
+export const CityDetails = ({ weatherInfo }) => {
+  const data = weatherInfo || {}
+  const { name, main, sys, weather, wind, visibility, clouds } = data || {}
+
+  const cityName = name
+  const currentTemp = main && Math.round(main.temp)
+  const feelsLikeTemp = main && Math.round(main.feels_like)
+  const maxTemp = main && Math.round(main.temp_max)
+  const minTemp = main && Math.round(main.temp_min)
+  const country = sys && sys.country
+  const weatherIconName = weather && weather[0].main
+  const weatherIconDesc = weather && weather[0].description
+  const visibilityInFt = visibility
+  const windSpeed = wind && wind.speed
+  const cloundPercent = clouds && clouds.all
+  const humidityPercent = main && main.humidity
+
+
+
+  return (
+    <div>
+      <Header />
+      <div>
+        <h2>{weatherIconName}</h2>
+        <h1>{currentTemp}</h1>
+        <h2>{cityName}</h2>
+      </div>
+      <div>
+        <WeatherCard type='feelLike' info={feelsLikeTemp} weatherIconDesc={weatherIconDesc} />
+        <WeatherCard type='max' info={maxTemp} weatherIconDesc={weatherIconDesc} />
+        <WeatherCard type='min' info={minTemp} weatherIconDesc={weatherIconDesc} />
+        <WeatherCard type='country' info={country} weatherIconDesc={weatherIconDesc} />
+      </div>
+    </div>
+  )
+}
---
+++ src/components/WeatherCard/WeatherCard.jsx
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react"
+
+
+export const WeatherCard = ({type, info, weatherIconDesc}) => {
+  const [title, setTitle] = useState('')
+
+  const getTitle = (type) => {
+    switch (type) {
+      case 'feelLike':
+        setTitle('Feels like');
+        break;
+      case 'max':
+        setTitle('Max Temp');
+        break;
+      case 'min':
+        setTitle('Min Temp');
+        break;
+      case 'country':
+        setTitle('Country');
+        break;
+      default:
+        setTitle('')
+        break;
+    }
+  }
+
+  useEffect(() => {
+    getTitle(type)
+  },[type])
+
+  return (
+    <div>
+      <h3>{title}</h3>
+      <h2>{info}</h2>
+      <h3>{weatherIconDesc}</h3>
+    </div>
+  )
+}
