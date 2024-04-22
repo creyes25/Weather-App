60a5c83931f0aaa5e32cb6529bfc66d9e32d653f
creyes25 <carolina.reyes2022@gmail.com>
2024-04-22 03:22:05 -0500

    add feature to call api based on city and display info

--- src/pages/City/City.jsx
+++ src/pages/City/City.jsx
@@ -1,8 +1,51 @@
+import { useState,useEffect } from "react"
+import { useParams } from "react-router-dom"
+import axios from 'axios'
+
+import { CityDetails } from "../../components/CityDetails/CityDetails"
+
+
+const baseUrl = `https://api.openweathermap.org/data/2.5/weather?q=`
+
+const apiId = 'c280715d3252b3470097491122ae9ce1'
 
 const City = () => {
+  let {cityId} = useParams()
+  cityId = cityId.toLowerCase()
+
+  const [weatherInfo, setWeatherInfo] = useState({})
+  const [isLoading, setIsLoading] = useState(false)
+  const [error , setError] = useState(false)
+
+
+  const getCityWeather = async () => {
+    try {
+      setError(false)
+      setIsLoading(true)
+      const {data} = await axios(`${baseUrl}${cityId}&appid=${apiId}&units=imperial`)
+      setWeatherInfo(data)
+      setIsLoading(false)
+    } catch (error) {
+      setIsLoading(false)
+      setError(true)
+    }
+  }
+
+  useEffect(()=> {
+    getCityWeather()
+  }, [cityId])
+
+
+
   return (
     <div>
-      City
+      {isLoading && <div>Is Loading</div>}
+      {error 
+        ? 
+      <h1>There was an error</h1> 
+        : 
+      <CityDetails weatherInfo={weatherInfo} error={error} />}
+      
     </div>
   )
 }
