7fd7e8f0e9311961b2acd45e7ffa21ce47089ebc
creyes25 <carolina.reyes2022@gmail.com>
2024-05-27 16:21:14 -0500

    cleaned up code

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
-import './App.css';
 
 import Navigation from './components/Navigation/Navigation';
 import Home from './pages/Home/Home';
